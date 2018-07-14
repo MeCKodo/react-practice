1d19d7a2cf14dd1bccdf971f3d40aebcb1d0bbcd
kodo <524580860@qq.com>
2018-07-14 17:13:23 +0800

    basic-example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import BasicExample from './basic-example/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const BasicLink = () => <Link to={'/basic'}>Basic</Link>;
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <h2>路由Demo</h2>
+      <BasicLink />
+      <Switch>
+        <Route path='/basic' component={BasicExample} />
+        <Route path='/' render={() => <h3>请选择一个想要看的例子</h3>} />
+      </Switch>
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
