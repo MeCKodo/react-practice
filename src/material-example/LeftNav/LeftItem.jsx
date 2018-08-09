35331dafeeb56c1206ac75c86fc8f24345e9c17a
MecKodo <524580860@qq.com>
2018-08-09 17:53:56 +0800

    badge replace

--- src/material-example/LeftNav/LeftItem.jsx
+++ src/material-example/LeftNav/LeftItem.jsx
@@ -4,15 +4,15 @@ import styled from 'styled-components';
 import MuiListItem from '@material-ui/core/ListItem';
 import MuiListItemText from '@material-ui/core/ListItemText';
 import {UmiBase, RedDotBase} from './UMI';
-import Badge from '@material-ui/core/Badge';
+import MuiBadge from '@material-ui/core/Badge';
 
 const Umi = styled(UmiBase)`
   position: absolute;
   top: 3px;
   left: 38px;
   opacity: ${props => props.open ? 0 : 1};
-  transition: opacity .35s ease;
-    transform: scale(.85);
+  transition: opacity .2s ease;
+  transform: scale(.85);
 `;
 
 const UmiRight = styled(UmiBase)`
@@ -110,17 +110,70 @@ const ListLink = styled(NavLink)`
     }
   }
 `;
-const IconWrapper = styled.div`
-    display: flex;
-    align-items: center;
+const Badge = styled(MuiBadge)`
+    //display: flex;
+    //align-items: center;
+    span {
+      ${props => !props.num ? `
+        left: 20px;
+        top: -4px;
+        height: 8px;
+        width: 8px;
+        border-radius:50%
+      ` : `
+        top: -8px;
+        left: 18px;
+        right: auto;
+        width: auto;
+        height: auto;
+        padding: 1px 5px;
+        border-radius: 12px;
+        transform: scale(.85);
+      `};
+      opacity: ${props => props.open ? 0 : 1};
+      transition: opacity .2s ease;
+      background: #FF8800;
+      border: 1px solid #fff;
+      color: #fff !important;
+      box-sizing: border-box;
+    }
 `;
-
+const RightBadge = styled(MuiBadge)`
+    span {
+      ${props => !props.num ? `
+        right: 0px;
+        top: -5px;
+        height: 8px;
+        width: 8px;
+        border-radius:50%
+      ` : `
+        top: -10px;
+    right: -3px;
+        width: auto;
+        height: auto;
+        padding: 1px 5px;
+        border-radius: 12px;
+        transform: scale(.85);
+      `};
+      opacity: ${props => props.open ? 0 : 1};
+      transition: opacity .2s ease;
+      background: #FF8800;
+      border: 1px solid #fff;
+      color: #fff !important;
+      box-sizing: border-box;
+    }
+    
+`;
 export default withRouter((props) => {
   const { open, url, type, icon : Icon, title, num } = props;
   // In order to make sure if active state it will be active's color
   // or hover's color
   const bgColor = props.location.pathname.indexOf(url) > -1 ? '#EBF6FA' : '#F5F5F5'; // Ice and 100
-  
+  const badge = num ? {
+    badge: 'umi'
+  } : {
+    badge: 'red-dot'
+  };
   return (
     <ListItem
       tabIndex='-1'
@@ -131,19 +184,33 @@ export default withRouter((props) => {
       color={bgColor}
       open={open}>
       <ListLink color={bgColor}  to={`/material/${url}`}>
-        <IconWrapper>
+        <Badge num={num}
+               classes={badge}
+               open={open}
+               badgeContent={num ? num : ''}
+               color="primary">
           <Icon/>
-          {type === 'red' ?
-            <RedDot open={open}/> :
-            (type === 'umi' ? <Umi open={open}>{num}</Umi> : null)
-          }
-        </IconWrapper>
+        </Badge>
         <ListItemText primary={`${title}`} open={open}/>
-        {type === 'red' ?
-          <RedDotRight open={open}/> :
-          (type === 'umi' ? <UmiRight open={open}>{num}</UmiRight> : null)
-        }
+        <RightBadge num={num}
+               classes={badge}
+               open={!open}
+               badgeContent={num ? num : ''}
+               color="primary">
+           <i></i>
+        </RightBadge>
       </ListLink>
     </ListItem>
   )
 });
+/**
+ {type === 'red' ?
+            <RedDot open={open}/> :
+            (type === 'umi' ? <Umi open={open}>{num}</Umi> : null)
+          }
+ 
+ {type === 'red' ?
+          <RedDotRight open={open}/> :
+          (type === 'umi' ? <UmiRight open={open}>{num}</UmiRight> : null)
+        }
+ **/
