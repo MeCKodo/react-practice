072d854a3691ca8e4732c31e6e83cc29f0ecfe7c
kodo <524580860@qq.com>
2019-02-16 22:01:09 +0800

    other type hooks

---
+++ src/hooks-example/hooks-types.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+function HookWithString() {
+  const [string, setString] = useState('abcde');
+
+  return (
+    <div>
+      <p>Render string is {string} </p>
+      <button onClick={() => setString('zxcvb')}>Click me</button>
+    </div>
+  );
+}
+
+function HookWithArray() {
+  const [array, setArray] = useState(['a', 'b']);
+
+  return (
+    <div>
+      <p>Render array is {array.toString()} </p>
+      <button onClick={() => setArray(array.concat('c'))}>Click me</button>
+    </div>
+  );
+}
+
+function HookWithObject() {
+  const [data, setData] = useState({ name: '二哲', age: 18 });
+
+  return (
+    <div>
+      <p>
+        Render name is {data.name}, age is {data.age}
+      </p>
+      <button onClick={() => setData({ ...data, name: '二哲儿' })}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+const Wrapper = React.memo(() => {
+  return (
+    <div>
+      <HookWithString />
+      <HookWithArray />
+      <HookWithObject />
+    </div>
+  );
+});
+
+export default Wrapper;
--- src/hooks-example/index.js
+++ src/hooks-example/index.js
@@ -1,7 +1,8 @@
-import React, { Suspense } from "react";
-import { Route, Link, Switch } from "react-router-dom";
+import React, { Suspense } from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
 
-import SimpleExampleHooks from "./simple-example-hooks";
+import SimpleExampleHooks from './simple-example-hooks';
+import HooksTypes from './hooks-types';
 
 function HookWrapper({ match }) {
   return (
